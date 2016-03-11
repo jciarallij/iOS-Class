1b8660a0b954599461c527b82fd3a254d87c0e89
jciarallij <jciarallij@gmail.com>
2016-03-10 21:27:38 -0500

    init commit

--- Lesson5/ContactListViewController.swift
+++ Lesson5/ContactListViewController.swift
@@ -10,12 +10,44 @@ import UIKit
 
 class ContactListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var contacts : [Contact]?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.contacts = DataManager.sharedManager.loadContacts()
 
         // Do any additional setup after loading the view.
     }
 
+    func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            return (self.contacts?.count )!
+    }
+    
+    
+    func tableView(tableView: UITableView,
+        cellforRowAtIndexPath indexPath: NSIndexPath ->
+        UITableViewCell {
+        
+        let contact = self.contacts![indexPath.row]
+        
+        let cell.textLabel?.text = "fred"
+        
+        return cell
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
